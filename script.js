751696a7e098119046df4d076687602eabaf3237
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2016-04-03 16:09:48 -0400

    Added a fallback incase of no Web Torrent
    Created a fallback linking to "The Art of War" by Sun Tzu (Translated by Lionel Giles. Read by Moira Fogarty.), if no Web Torrent is available.

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ navigator.registerProtocolHandler("web+magnetmusic", "/#%s", "Web Magnet Music")
 
 if (WebTorrent.WEBRTC_SUPPORT) {
   console.log('Web Torrent is supported!');
-  if(window.location.hash){ loadTorrent(location.hash); console.log('Got Web Torrent!'); } else { playerEle.innerHTML="No Web Torrent given to load. ☹️"; }
+  if(window.location.hash){ loadTorrent(location.hash); console.log('Got Web Torrent!'); } else { playerEle.innerHTML="No Web Torrent given to load. ☹️. <br/><a href='/#magnet:?xt=urn:btih:b260fa9dc51093bd20d31ca9ccfa3c3abf157a13&dn=art_of_war_librivox&tr=http%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=http%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=http%3A%2F%2Fia600508.us.archive.org%2F19%2Fitems%2F&ws=http%3A%2F%2Fia700508.us.archive.org%2F19%2Fitems%2F&ws=https%3A%2F%2Farchive.org%2Fdownload%2F'>Try an audiobook of the Art of War by Sun Tzu (Translated by Lionel Giles. Read by Moira Fogarty.)</a>."; }
 } else {
   playerEle.innerHTML="Sorry. Web Torrent isn't supported in your browser. ☹️";
 }
