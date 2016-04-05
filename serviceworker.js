1cb2234b60ff01081f11ac355cf06a3c408c4c98
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2016-04-04 23:53:31 -0400

    Removed /WebTorrentClient
    Removed /WebTorrentClient from the cache as it can't be cached, only /WebTorrentClient/.

--- serviceworker.js
+++ serviceworker.js
@@ -3,8 +3,7 @@ var appVersion = '0.0.1';
 var CACHE_NAME = 'sw-cache';
 var urlsToCache = [
   'index.html',
-  'WebTorrentClient/',
-  'WebTorrentClient',
+  '/WebTorrentClient/',
   'modernizr-custom.js',
   'webtorrent.min.js',
   'playlist-parser.min.js',
